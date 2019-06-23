2a51029fdfcc1dcf9d61fc6514bfbc14ab2f6184
MikeSquall <michael.arbib@om2.com>
2019-06-23 12:30:03 +0300

    Added a test package to check deployment

---
+++ packages/service-test-ci/cypress/integration/template.test.ts
@@ -0,0 +1,28 @@
+import '../support';
+import bootstrap from '../../src/index';
+import TestService from '../../src/Service';
+import mocks from '../support/mocks';
+
+describe('Service Template TCs', () => {
+  const defaultConfig = { message: 'Hello' };
+
+  it('Bootstrap registers component correctly', async () => {
+    const registerServiceStub = cy.stub();
+    const fakeWorkspace = mocks.getWorkspaceMock(registerServiceStub);
+    await bootstrap(fakeWorkspace, defaultConfig);
+
+    expect(registerServiceStub).to.be.calledOnce;
+    // @ts-ignore
+    const registerServiceRequest = registerServiceStub.args[0][0];
+    expect(Object.keys(registerServiceRequest).length).to.equal(2);
+    expect(typeof registerServiceRequest.serviceName === 'string' && !!registerServiceRequest.serviceName).to.be.true;
+    expect(registerServiceRequest.reference instanceof TestService).to.be.true;
+  });
+
+  it('TestService shows a message', async () => {
+    const testService = new TestService(defaultConfig.message);
+    return testService.showMessage().then((response: string) => {
+      expect(response).to.equal(`response from Service: ${defaultConfig.message}`);
+    });
+  });
+});
---
+++ packages/service-test-ci/example/index.js
@@ -0,0 +1,2 @@
+const container = document.getElementById('web-test');
+container.innerText = 'Hello World ! This is a test';
