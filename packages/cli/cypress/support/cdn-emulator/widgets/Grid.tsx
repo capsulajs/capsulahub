956d069ff5d6e32ea4d19a08b3e3bbfc7ec09602
dmitriyslaym <dmitriyslaym@gmail.com>
2019-06-25 17:44:48 +0300

    TODO Prepare descriptive small extensions

---
+++ packages/cli/cypress/support/cdn-emulator/widgets/Grid.tsx
@@ -0,0 +1,24 @@
+import { API } from '@capsulajs/capsulahub-workspace';
+
+declare var publicExports: object;
+
+const bootstrap = (_: API.Workspace, config: { title: string }) => {
+  return new Promise((resolve) => {
+    class Grid extends HTMLElement {
+      constructor() {
+        super();
+        this.innerHTML = `<div id="grid"><h1>Capsulahub title: ${
+          config.title
+        }</h1><div id="web-component-a"></div></div>`;
+      }
+    }
+
+    resolve(Grid);
+  });
+};
+
+if (typeof publicExports !== 'undefined') {
+  publicExports = bootstrap;
+}
+
+export default bootstrap;
--- packages/cli/webpack.config.js
+++ packages/cli/webpack.config.js
@@ -4,10 +4,10 @@ const baseConfig = require('../../webpack.config');
 module.exports = (env) => ({
   ...baseConfig(env),
   entry: {
-    'port-1111/services/serviceA': './cypress/support/cdn-emulator/port-1111/services/serviceA.ts',
-    'port-1111/services/serviceB': './cypress/support/cdn-emulator/port-1111/services/serviceB.ts',
-    'port-1111/widgets/Grid': './cypress/support/cdn-emulator/port-1111/widgets/Grid.tsx',
-    'port-1111/widgets/RequestForm': './cypress/support/cdn-emulator/port-1111/widgets/RequestForm.tsx',
+    'services/serviceA': './cypress/support/cdn-emulator/services/serviceA.ts',
+    'services/serviceB': './cypress/support/cdn-emulator/services/serviceB.ts',
+    'widgets/Grid': './cypress/support/cdn-emulator/widgets/Grid.tsx',
+    'widgets/RequestForm': './cypress/support/cdn-emulator/widgets/RequestForm.tsx',
   },
   output: {
     path: path.resolve(__dirname, 'cdn-emulator'),
