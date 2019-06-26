592924144532de458bafe98470eeea154b8a37ad
dmitriyslaym <dmitriyslaym@gmail.com>
2019-06-26 13:58:09 +0300

    Implemented extensions

---
+++ packages/cli/cypress/support/cdn-emulator/widgets/ComponentA.tsx
@@ -0,0 +1,43 @@
+import { BehaviorSubject, Observable } from 'rxjs';
+import { API } from '@capsulajs/capsulahub-workspace';
+
+declare var publicExports: object;
+
+const messageId = 'component-a-message';
+
+const bootstrap = (_: API.Workspace, config: { name: string }) => {
+  return new Promise((resolve) => {
+    class ComponentA extends HTMLElement {
+      private props$: BehaviorSubject<{ message: string }>;
+
+      constructor() {
+        super();
+        this.innerHTML = `
+          <div id="component-a">
+            <h2>Hello from ${config.name}</h2>
+            <h3 id="${messageId}"></h3>
+          </div>
+        `;
+
+        this.props$ = new BehaviorSubject({ message: '' });
+      }
+
+      connectedCallback() {
+        this.props$.subscribe((props: { message: string }) => {
+          document.getElementById(messageId)!.innerText = props.message;
+        });
+      }
+
+      public setProps(additionalProps$: Observable<{ message: string }>) {
+        additionalProps$.subscribe((props: { message: string }) => this.props$.next(props));
+      }
+    }
+    resolve(ComponentA);
+  });
+};
+
+if (typeof publicExports !== 'undefined') {
+  publicExports = bootstrap;
+}
+
+export default bootstrap;
