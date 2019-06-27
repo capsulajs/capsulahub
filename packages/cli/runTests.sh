85d0885ee5c712ed3da7f4ac88e5cc888d4a635a
dmitriyslaym <dmitriyslaym@gmail.com>
2019-06-27 18:08:20 +0300

    Run all build commands

--- packages/cli/runTests.sh
+++ packages/cli/runTests.sh
@@ -12,14 +12,14 @@ BIN='../../node_modules/.bin/'
 nohup "$BIN"http-server cdn-emulator -p 1111 --cors &>/dev/null &
 pid_server_1111=$!
 
-## Success scenario #1
+## Run Success scenario #1
 ## capsulahub run --token=http://localhost:1111/port-1111/configuration --configProvider=httpFile --port=8888
 nohup "$BIN"capsulahub run --token=http://localhost:1111/port-1111/configuration --configProvider=httpFile --port=8888 &>/dev/null &
 pid_server_8888=$!
 "$BIN"cypress run --browser chrome --spec "cypress/integration/capsulahub_run/success/1-capsulahub_run.test.js"
 kill "$pid_server_8888"
 
-## Success scenario #2
+## Run Success scenario #2
 
 ## localStorage configProvider
 ## capsulahub run --token=configuration --configProvider=localStorage --port=7777
@@ -35,14 +35,14 @@ pid_server_7778=$!
 "$BIN"cypress run --browser chrome --spec "cypress/integration/capsulahub_run/success/2-capsulahub_run-http-server.test.js"
 kill "$pid_server_7778"
 
-## Success scenario #3
+## Run Success scenario #3
 ## capsulahub run --token=http://localhost:1111/port-1111/configuration
 nohup "$BIN"capsulahub run --token=http://localhost:1111/port-1111/configuration &>/dev/null &
 pid_server_55555=$!
 "$BIN"cypress run --browser chrome --spec "cypress/integration/capsulahub_run/success/3-capsulahub_run.test.js"
 kill "$pid_server_55555"
 
-## Success scenario #4
+## Run Success scenario #4
 ## capsulahub run --token=http://localhost:1111/port-1111/configuration --port=1234
 nohup "$BIN"capsulahub run --token=http://localhost:1111/port-1111/configuration --port=1234 &>/dev/null &
 pid_server_1234=$!
@@ -53,7 +53,7 @@ pid_server_4321=$!
 kill "$pid_server_1234"
 kill "$pid_server_4321"
 
-## Success scenario #5
+## Run Success scenario #5
 
 ## Preparing CDN emulation for extensions and configuration for HTTPFile provider on PORT 4444
 "$BIN"cpy cypress/fixtures/port-4444-httpFile/workspace.json cdn-emulator/port-4444/configuration
@@ -70,12 +70,18 @@ pid_server_4321=$!
 kill "$pid_server_1234"
 kill "$pid_server_4321"
 
-## Negative scenario #4
+## Run Negative scenario #4
 ## capsulahub run --token=http://localhost:1111/port-1111/configuration --port=1234
 nohup "$BIN"capsulahub run --token=http://localhost:1111/port-1111/configuration --port=8888 &>/dev/null &
 pid_server_8888=$!
 "$BIN"cypress run --browser chrome --spec "cypress/integration/capsulahub_run/negative/4-capsulahub_run.test.js"
 kill "$pid_server_8888"
+
+## Run Negative scenario others
+"$BIN"cypress run --browser chrome --spec "cypress/integration/capsulahub_run/negative/capsulahub_run.test.js"
+
+## Build scenarios
+"$BIN"cypress run --browser chrome --spec "cypress/integration/capsulahub_build.test.js"
 
 ## Close CDN-emulator
 kill "$pid_server_1111"
