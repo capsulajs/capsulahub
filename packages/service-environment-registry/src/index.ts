6184feedf9b653feb28eb902d9f0c02b63c953af
Michael Arbib <michael.arbib@om2.com>
2019-07-03 10:58:29 +0300

    [Feature] Create Environment registry extension for CapsulaHub (#15)
    * Started env registry extension
    
    * Fixed config + tests
    
    * Added dynamic serviceName in config
    
    * Updated to follow new tpl syntax
    
    * Fixed tests
    
    * Fixed README
    
    * Fixed linting
    
    * Fixed test

---
+++ packages/service-environment-registry/src/index.ts
@@ -0,0 +1,26 @@
+import { API as WORKSPACE_API } from '@capsulajs/capsulahub-workspace';
+import * as API from './api';
+import utils from './helpers/utils';
+
+declare let publicExports: object;
+
+const bootstrap = (WORKSPACE: WORKSPACE_API.Workspace, SERVICE_CONFIG: API.EnvironmentRegistryConfig) => {
+  return new Promise((resolve) => {
+    const { serviceName } = SERVICE_CONFIG;
+
+    const registerServiceData = {
+      serviceName,
+      reference: utils.getServiceInstance(SERVICE_CONFIG),
+    };
+
+    WORKSPACE.registerService(registerServiceData);
+    resolve();
+  });
+};
+
+if (typeof publicExports !== 'undefined') {
+  publicExports = bootstrap;
+}
+
+export { API };
+export default bootstrap;
