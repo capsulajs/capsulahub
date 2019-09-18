255a3f59e79be031f90fb63b8a1513fb2ccd159c
dmitriyslaym <dmitriyslaym@gmail.com>
2019-09-18 18:11:07 +0300

    Fix canvas tests

---
+++ packages/widget-canvas/src/api/OnUpdateEvent.ts
@@ -0,0 +1,12 @@
+import { Layout } from './index';
+
+interface UpdatedLayout {
+  layout: Layout;
+}
+
+export type EventType = 'drop' | 'resize' | 'resizestop' | 'reorder' | 'move' | 'select' | 'update' | 'remove';
+
+export interface OnUpdateEvent extends UpdatedLayout {
+  event: EventType;
+  data?: any;
+}
