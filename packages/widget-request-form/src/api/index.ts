9972fcbd365322685ddffcc145988788b3220928
Dmitriy <dmitriyslaym@gmail.com>
2019-10-08 15:12:35 +0300

    [API] Additional options for request form (#53)
    * [API] Additional options for request form
    
    * Fix linting
    
    * Make some options optional in RequestForm API
    
    * Added label and fieldName to AdditionalOptions

--- packages/widget-request-form/src/api/index.ts
+++ packages/widget-request-form/src/api/index.ts
@@ -1,5 +1,6 @@
 import SubmittedData from './SubmittedData';
 import RequestFormUIProps from './RequestFormUIProps';
 import Language from './Language';
+import AdditionalOptions, { Option } from './AdditionalOptions';
 
-export { SubmittedData, RequestFormUIProps, Language };
+export { SubmittedData, RequestFormUIProps, Language, AdditionalOptions, Option };
