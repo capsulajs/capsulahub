a3e7f70fa6a25b3dd14251d24818ff47181f5420
dmitriyslaym <dmitriyslaym@gmail.com>
2019-10-28 13:17:37 +0200

    Added user info

--- packages/service-auth/src/api/User.ts
+++ packages/service-auth/src/api/User.ts
@@ -7,4 +7,23 @@ export interface User {
    */
   token: string;
   name?: string;
+  givenName?: string;
+  familyName?: string;
+  middleName?: string;
+  nickname?: string;
+  preferredUsername?: string;
+  profile?: string;
+  picture?: string;
+  website?: string;
+  email?: string;
+  isEmailVerified?: boolean;
+  gender?: string;
+  birthDate?: string;
+  zoneInfo?: string;
+  locale?: string;
+  phoneNumber?: string;
+  isPhoneNumberVerified?: boolean;
+  address?: string;
+  updatedAt?: string;
+  expirationTime?: number;
 }
